9f6ab53d8256853fba45b5e7a11ab6c136bfcae1
Matthew Boyette <Dyndrilliac@gmail.com>
2016-03-10 19:39:35 -0500

    Whitespace Formatting.

--- PathfinderDemo.java
+++ PathfinderDemo.java
@@ -19,119 +19,122 @@ import api.util.stdlib.StdOut;
 
 public class PathfinderDemo
 {
-	public static final void main(final String[] args)
-	{
-		new PathfinderDemo(args);
-	}
-	
-	private boolean				debugMode	= false;
-	private ApplicationWindow	window		= null;
-	
-	public PathfinderDemo(final String[] args)
-	{
-		this.setDebugging(Support.promptDebugMode(this.getWindow()));
-		
-		if (args.length > 0)
-		{
-			final AStarShortestPath2D shortestPath = new AStarShortestPath2D(this.isDebugging(), args[0]);
-			
-			if (this.isDebugging())
-			{
-				StdOut.println("Start point: " + shortestPath.getStartPoint().toString());
-				StdOut.println("Goal point: " + shortestPath.getGoalPoint().toString());
-				
-				StdOut.println("\nVertices (" + shortestPath.getVertices().size() + "):");
-				
-				for (int j = 0; j < shortestPath.getVertices().size(); j++)
-				{
-					StdOut.println(shortestPath.getVertices().get(j));
-				}
-				
-				StdOut.println("\nEdges (" + shortestPath.getEdges().size() + "):");
-				
-				for (int j = 0; j < shortestPath.getEdges().size(); j++)
-				{
-					StdOut.println(shortestPath.getEdges().get(j));
-				}
-				
-				if (shortestPath.getGoalPoint().isWithinLineOfSightOf(shortestPath.getStartPoint(), 
-					shortestPath.getEdges()))
-				{
-					StdOut.println("\nThe goal point is within line of sight of the start point.");
-				}
-				else
-				{
-					StdOut.println("\nThe goal point is not within line of sight of the start point.");
-				}
-				
-				StdOut.println("\nVertices adjacent to start point (" + 
-					shortestPath.getStartPoint().getNodesWithinLOS().size() + "):");
-				
-				for (int j = 0; j < shortestPath.getStartPoint().getNodesWithinLOS().size(); j++)
-				{
-					StdOut.println(shortestPath.getStartPoint().getNodesWithinLOS().get(j));
-				}
-				
-				StdOut.println("\nVertices adjacent to goal point (" + 
-					shortestPath.getGoalPoint().getNodesWithinLOS().size() + "):");
-				
-				for (int j = 0; j < shortestPath.getGoalPoint().getNodesWithinLOS().size(); j++)
-				{
-					StdOut.println(shortestPath.getGoalPoint().getNodesWithinLOS().get(j));
-				}
-			}
-			
-			if (!shortestPath.getSolution().isEmpty())
-			{
-				StdOut.println("\nSolution:");
-				
-				for (int i = 0; i < shortestPath.getSolution().size(); i++)
-				{
-					StdOut.println(shortestPath.getSolution().get(i).toString() + "\n");
-				}
-			}
-			
-			// Define a self-contained interface construction event handler.
-			EventHandler<PathfinderDemo> myDrawGUI = new EventHandler<PathfinderDemo>(this)
-			{
-				private final static long	serialVersionUID	= 1L;
-				
-				@Override
-				public final void run(final ApplicationWindow window)
-				{
-					Container contentPane = window.getContentPane();
-					AStarShortestPath2D.MapDisplay mapDisplay = new AStarShortestPath2D.MapDisplay(Color.WHITE, 
-						Color.BLACK, shortestPath, 10.0);
-					
-					contentPane.setLayout(new BorderLayout());
-					contentPane.add(mapDisplay, BorderLayout.CENTER);
-					mapDisplay.repaint();
-				}
-			};
-			
-			this.setWindow(new ApplicationWindow(null, "A* Pathfinding Application", new Dimension(425, 250), this.isDebugging(), 
-				false, null, myDrawGUI));
-			this.getWindow().setIconImageByResourceName("icon.png");
-		}
-	}
-	
-	public final ApplicationWindow getWindow()
-	{
-		return this.window;
-	}
-	
-	public final boolean isDebugging()
-	{
-		return this.debugMode;
-	}
-	
-	protected final void setDebugging(final boolean debugMode)
-	{
-		this.debugMode = debugMode;
-	}
-	
-	protected final void setWindow(final ApplicationWindow window)
-	{
-		this.window = window;
-	}
+    public static final void main(final String[] args)
+    {
+        new PathfinderDemo(args);
+    }
+    
+    private boolean           debugMode = false;
+    private ApplicationWindow window    = null;
+    
+    public PathfinderDemo(final String[] args)
+    {
+        this.setDebugging(Support.promptDebugMode(this.getWindow()));
+        
+        if (args.length > 0)
+        {
+            final AStarShortestPath2D shortestPath = new AStarShortestPath2D(this.isDebugging(), args[0]);
+            
+            if (this.isDebugging())
+            {
+                StdOut.println("Start point: " + shortestPath.getStartPoint().toString());
+                StdOut.println("Goal point: " + shortestPath.getGoalPoint().toString());
+                
+                StdOut.println("\nVertices (" + shortestPath.getVertices().size() + "):");
+                
+                for (int j = 0; j < shortestPath.getVertices().size(); j++)
+                {
+                    StdOut.println(shortestPath.getVertices().get(j));
+                }
+                
+                StdOut.println("\nEdges (" + shortestPath.getEdges().size() + "):");
+                
+                for (int j = 0; j < shortestPath.getEdges().size(); j++)
+                {
+                    StdOut.println(shortestPath.getEdges().get(j));
+                }
+                
+                if (shortestPath.getGoalPoint().isWithinLineOfSightOf(shortestPath.getStartPoint(), shortestPath.getEdges()))
+                {
+                    StdOut.println("\nThe goal point is within line of sight of the start point.");
+                }
+                else
+                {
+                    StdOut.println("\nThe goal point is not within line of sight of the start point.");
+                }
+                
+                StdOut.println("\nVertices adjacent to start point (" + shortestPath.getStartPoint().getNodesWithinLOS().size() + "):");
+                
+                for (int j = 0; j < shortestPath.getStartPoint().getNodesWithinLOS().size(); j++)
+                {
+                    StdOut.println(shortestPath.getStartPoint().getNodesWithinLOS().get(j));
+                }
+                
+                StdOut.println("\nVertices adjacent to goal point (" + shortestPath.getGoalPoint().getNodesWithinLOS().size() + "):");
+                
+                for (int j = 0; j < shortestPath.getGoalPoint().getNodesWithinLOS().size(); j++)
+                {
+                    StdOut.println(shortestPath.getGoalPoint().getNodesWithinLOS().get(j));
+                }
+            }
+            
+            if (!shortestPath.getSolution().isEmpty())
+            {
+                StdOut.println("\nSolution:");
+                
+                for (int i = 0; i < shortestPath.getSolution().size(); i++)
+                {
+                    StdOut.println(shortestPath.getSolution().get(i).toString() + "\n");
+                }
+            }
+            
+            // Define a self-contained interface construction event handler.
+            // @formatter:off
+            EventHandler<PathfinderDemo> myDrawGUI = new EventHandler<PathfinderDemo>(this)
+            {
+                private final static long serialVersionUID = 1L;
+
+                @Override
+                public final void run(final ApplicationWindow window)
+                {
+                    Container contentPane = window.getContentPane();
+                    AStarShortestPath2D.MapDisplay mapDisplay = new AStarShortestPath2D.MapDisplay(Color.WHITE, Color.BLACK, shortestPath, 10.0);
+
+                    contentPane.setLayout(new BorderLayout());
+                    contentPane.add(mapDisplay, BorderLayout.CENTER);
+                    mapDisplay.repaint();
+                }
+            };
+
+            this.setWindow(new ApplicationWindow(null,
+                "A* Pathfinding Application",
+                new Dimension(425, 250),
+                this.isDebugging(),
+                false,
+                null,
+                myDrawGUI));
+            this.getWindow().setIconImageByResourceName("icon.png");
+            // @formatter:on
+        }
+    }
+    
+    public final ApplicationWindow getWindow()
+    {
+        return this.window;
+    }
+    
+    public final boolean isDebugging()
+    {
+        return this.debugMode;
+    }
+    
+    protected final void setDebugging(final boolean debugMode)
+    {
+        this.debugMode = debugMode;
+    }
+    
+    protected final void setWindow(final ApplicationWindow window)
+    {
+        this.window = window;
+    }
 }
